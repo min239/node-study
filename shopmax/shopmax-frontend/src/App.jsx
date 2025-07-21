391c3e5dc3b810f427656200459de1719397f8c9
dtkd19 <tkdgns9804@naver.com>
2025-07-21 18:03:22 +0900

    up

---
+++ shopmax/shopmax-frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import './styles/common.css'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
+import Navbar from './components/shared/Navbar'
+import Footer from './components/shared/Footer'
+import Home from './pages/Home'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import { checkAuthStatusThunk } from './features/authSlice'
+
+function App() {
+   const dispatch = useDispatch()
+   const { isAuthenticated, user } = useSelector((state) => state.auth) //로그인 상태, 로그인 한 사용자 정보
+
+   // 새로고침시 지속적인 로그인 상태 확인을 위해 사용
+   useEffect(() => {
+      dispatch(checkAuthStatusThunk())
+   }, [dispatch])
+
+   return (
+      <>
+         <Navbar isAuthenticated={isAuthenticated} user={user} />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+         </Routes>
+         <Footer />
+      </>
+   )
+}
+
+export default App
